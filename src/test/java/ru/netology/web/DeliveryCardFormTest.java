ced905d7bf0444dc6c6204f50a0a23c264b97838
Елена Мурашева <vacilika77@gmail.com>
2022-08-25 23:59:43 +0300

    Modified tests

--- src/test/java/ru/netology/web/DeliveryCardFormTest.java
+++ src/test/java/ru/netology/web/DeliveryCardFormTest.java
@@ -1,14 +1,14 @@
 package ru.netology.web;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.text.SimpleDateFormat;
 import java.time.Duration;
-import java.util.Calendar;
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
@@ -24,24 +24,21 @@ class DeliveryCardFormTest {
 
     WebDriver driver;
 
+    public String generateDate(int days) {
+        return
+                LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
 
     @Test
     void shouldRegisterTest1() {
         open("http://localhost:9999");
         Configuration.holdBrowserOpen = true;
-
-        // устанавливаем текущую дату ПЛЮС 3 дня
-        Date currentDate = new Date();
-        Calendar c = Calendar.getInstance();
-        c.setTime(currentDate);
-        c.add(Calendar.DATE, 3);
-        SimpleDateFormat formatForCurrentDate = new SimpleDateFormat("dd.MM.y");
-        String newCurrentDate = formatForCurrentDate.format(c.getTime());
+        String planningDate = generateDate(5);
 
         //заполняем форму, делаем проверку
         $("div [data-test-id='city'] input").setValue("Иркутск");
         $("div [data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
-        $("div [data-test-id='date'] input").setValue(newCurrentDate);
+        $("div [data-test-id='date'] input").setValue(planningDate);
         $("div [data-test-id='name'] input").setValue("Пелагея Мамин-Сибиряк");
         $("div [data-test-id='phone'] input").setValue("+79996663355");
         $$("[data-test-id='agreement']").last().click();
@@ -53,19 +50,12 @@ class DeliveryCardFormTest {
     void shouldRegisterTest2() {
         open("http://localhost:9999");
         Configuration.holdBrowserOpen = true;
-
-        // устанавливаем текущую дату ПЛЮС 3 дня
-        Date currentDate = new Date();
-        Calendar c = Calendar.getInstance();
-        c.setTime(currentDate);
-        c.add(Calendar.DATE, 3);
-        SimpleDateFormat formatForCurrentDate = new SimpleDateFormat("dd.MM.y");
-        String newCurrentDate = formatForCurrentDate.format(c.getTime());
+        String planningDate = generateDate(3);
 
         //заполняем форму, делаем проверку
         $("div [data-test-id='city'] input").setValue("Великий Новгород");
         $("div [data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
-        $("div [data-test-id='date'] input").setValue(newCurrentDate);
+        $("div [data-test-id='date'] input").setValue(planningDate);
         $("div [data-test-id='name'] input").setValue("Йорик Панкратов-Черный");
         $("div [data-test-id='phone'] input").setValue("+79126663355");
         $$("[data-test-id='agreement']").last().click();
@@ -77,24 +67,35 @@ class DeliveryCardFormTest {
     void shouldRegisterTest3() {
         open("http://localhost:9999");
         Configuration.holdBrowserOpen = true;
-
-        // устанавливаем текущую дату ПЛЮС 3 дня
-        Date currentDate = new Date();
-        Calendar c = Calendar.getInstance();
-        c.setTime(currentDate);
-        c.add(Calendar.DATE, 3);
-        SimpleDateFormat formatForCurrentDate = new SimpleDateFormat("dd.MM.y");
-        String newCurrentDate = formatForCurrentDate.format(c.getTime());
+        String planningDate = generateDate(6);
 
         //заполняем форму, делаем проверку
         $("div [data-test-id='city'] input").setValue("Петропавловск-Камчатский");
         $("div [data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
-        $("div [data-test-id='date'] input").setValue(newCurrentDate);
+        $("div [data-test-id='date'] input").setValue(planningDate);
         $("div [data-test-id='name'] input").setValue("Анна-Мария Ю");
         $("div [data-test-id='phone'] input").setValue("+79656663851");
         $$("[data-test-id='agreement']").last().click();
         $$(By.className("button__content")).last().click();
         $("div.notification__title").shouldHave(exactText("Успешно!"), Duration.ofSeconds(40));
+    }
+
+    @Test
+    void shouldRegisterTest4() {
+        open("http://localhost:9999");
+        Configuration.holdBrowserOpen = true;
+        String planningDate = generateDate(4);
+
+        //заполняем форму, делаем проверку
+        $("div [data-test-id='city'] input").setValue("Калининград");
+        $("div [data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("div [data-test-id='date'] input").setValue(planningDate);
+        $("div [data-test-id='name'] input").setValue("Елена Гончарук-Дугач");
+        $("div [data-test-id='phone'] input").setValue("+79996663351");
+        $$("[data-test-id='agreement']").last().click();
+        $$(By.className("button__content")).last().click();
+        $("div.notification__content").shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(20)).shouldBe(Condition.visible);
+
     }
 
 }
